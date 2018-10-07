d97fecfb5822a1a7a3bda3427b3ee149788523d2
HanchaoLi <seraphme1234@gmail.com>
2018-10-07 15:21:30 -0400

    @qualifier practice

--- String-demo-annotations/src/com/springdemo/TennisCoach.java
+++ String-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 //  also supports default, it is tennisCoach
@@ -10,6 +11,7 @@ public class TennisCoach implements Coach{
 	
 	//  field di
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	//	constructor di
 	/*
