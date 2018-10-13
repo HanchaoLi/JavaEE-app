16e52cbecce57de87a1779507b05c46151015a00
HanchaoLi <seraphme1234@gmail.com>
2018-10-13 16:04:39 -0400

    Java config file

--- String-demo-annotations/src/com/springdemo/SportConfig.java
+++ String-demo-annotations/src/com/springdemo/SportConfig.java
@@ -1,10 +1,22 @@
 package com.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.springdemo")
 public class SportConfig {
-
+	// define bean for our sad fortune service
+	
+	@Bean
+	public FortuneService sadFortuneSerivce() {
+		return new SadFortuneService();
+	}
+	
+	// defina bean for our swim coach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneSerivce());
+	}
 }
