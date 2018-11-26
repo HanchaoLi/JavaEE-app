c2a967b545dfcfed4e12cdd9c9c448b52996a62e
HanchaoLi <seraphme1234@gmail.com>
2018-11-25 20:00:40 -0500

    store string and object to redis

--- base_project/src/main/java/net/xdclass/base_project/controller/RedisTestController.java
+++ base_project/src/main/java/net/xdclass/base_project/controller/RedisTestController.java
@@ -1,5 +1,7 @@
 package net.xdclass.base_project.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.xdclass.base_project.domain.JsonData;
+import net.xdclass.base_project.domain.User;
+import net.xdclass.base_project.utils.JsonUtils;
 
 @RestController
 @RequestMapping("/api/v1/redis")
@@ -25,5 +29,23 @@ public class RedisTestController {
 	public Object get() {
 		String value = redisTpl.opsForValue().get("name");
 		return JsonData.buildSuccess(value);
+	}
+	
+	@GetMapping(value="setuser")
+	public Object setUser() {
+		User user = new User();
+		user.setName("lee");
+		user.setAge(11);
+		user.setCreateTime(new Date());
+		user.setPhone("3243423");
+		String userstr = JsonUtils.obj2String(user);
+		redisTpl.opsForValue().set("user:11", userstr);
+		return JsonData.buildSuccess("OK");
+	}
+	
+	@GetMapping(value="getuser")
+	public Object getUser() {
+		User user = JsonUtils.string2Obj(redisTpl.opsForValue().get("user:11"), User.class);
+		return JsonData.buildSuccess(user);
 	}
 }
