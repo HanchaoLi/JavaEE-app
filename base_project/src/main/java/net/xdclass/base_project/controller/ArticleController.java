a0e4d544bcfb42551e4ef0efce58f1137d46347c
HanchaoLi <seraphme1234@gmail.com>
2018-12-02 00:15:24 -0500

    elastic search 5.6 combination

---
+++ base_project/src/main/java/net/xdclass/base_project/controller/ArticleController.java
@@ -0,0 +1,42 @@
+package net.xdclass.base_project.controller;
+
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.xdclass.base_project.domain.Article;
+import net.xdclass.base_project.domain.JsonData;
+import net.xdclass.base_project.repository.ArticleRepository;
+
+@RestController
+@RequestMapping("/api/v1/article")
+public class ArticleController {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@GetMapping("save")
+	public Object save() {
+		Article article = new Article();
+		article.setId(1L);
+		article.setPv(123);
+		article.setContent("spring boot learning !");
+		article.setTitle("title 1");
+		article.setSummary("search");
+		
+		articleRepository.save(article);
+		return JsonData.buildSuccess();
+	}
+	
+	@GetMapping("search")
+	public Object search(String title) {
+		QueryBuilder builder = QueryBuilders.matchQuery("title", title);
+		
+		Iterable<Article> list = articleRepository.search(builder);
+		
+		return JsonData.buildSuccess(list);
+	}
+}
